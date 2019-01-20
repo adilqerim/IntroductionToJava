8a06e1abc2559c6bee4fb3dd7b3aad71e1e8341d
adiluser <46440216+adiluser@users.noreply.github.com>
2019-01-20 16:50:15 +0600

    Update FifthChapter03.java

--- chapter5/FifthChapter03.java
+++ chapter5/FifthChapter03.java
@@ -1,13 +1,11 @@
 public class FifthChapter13 {
 	public static void main(String[] args) {
-		int n = 0; 	// Intialize n at 0;
-
-		// Find the largest n such that n^3 is < 12,000
+		int n = 0; 	
 		while (Math.pow(n + 1, 3) < 12000) {
 			n++; 		// Increment n
 		}
 
-		// Display result
+	
 		System.out.println(
 			"Largest integer n such that n^3 is less than 12,000: " + n);
 	}
